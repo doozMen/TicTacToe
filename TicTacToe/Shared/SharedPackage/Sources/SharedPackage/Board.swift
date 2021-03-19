e95ec4f1ac4df98af36d86492b1efb4f498713bf
kata-me <kata@dooz.be>
2021-03-19 16:19:26 +0100

    add failing test for gameover check

--- TicTacToe/Shared/SharedPackage/Sources/SharedPackage/Board.swift
+++ TicTacToe/Shared/SharedPackage/Sources/SharedPackage/Board.swift
@@ -3,7 +3,10 @@ import Combine
 
 final class Board {
     let squares: [Square]
+    @Published var isGameOver: Bool
+    
     init() {
         self.squares = Array(repeating: .init(.nobody), count: 9)
+        self.isGameOver = false
     }
 }
--- TicTacToe/Shared/SharedPackage/Tests/SharedPackageTests/BoardTests.swift
+++ TicTacToe/Shared/SharedPackage/Tests/SharedPackageTests/BoardTests.swift
@@ -3,9 +3,28 @@ import XCTest
 @testable import SharedPackage
 
 final class BoardTests: XCTestCase {
-    func test_board_setup() {
-        let board = Board()
+    var board: Board!
+    override func setUp() {
+        super.setUp()
+        board = Board()
+    }
+    
+    func test_board_initial_nobody_on_any_square() {
         let result = board.squares.map(\.occupiedBy)
         XCTAssertEqual(result, Array(repeating: Square.OccupiedBy.nobody, count: 9))
+    }
+    
+    func test_board_gameover() throws {
+        var result = false
+        let cancelable = board
+            .$isGameOver
+            .sink { result = $0 }
+        
+        XCTAssertFalse(result, "initially the game should be ongoing")
+        
+        board.squares.first?.occupiedBy = .home
+        
+        XCTAssertTrue(result, "A game ends if there are no more boxes available or if one of the two players align three symbols before the other.")
+        cancelable.cancel()
     }
 }
