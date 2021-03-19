1e9974243ee54b13bee1de5562d827e860f6c439
kata-me <kata@dooz.be>
2021-03-19 17:22:59 +0100

    make gameover check succeed again

--- TicTacToe/Shared/SharedPackage/Tests/SharedPackageTests/BoardTests.swift
+++ TicTacToe/Shared/SharedPackage/Tests/SharedPackageTests/BoardTests.swift
@@ -24,32 +24,31 @@ final class BoardTests: XCTestCase {
         XCTAssertTrue(board.isStarted)
     }
     
-//    func test_board_gameover() throws {
-//        var result = false
-//        var callCount = 0
-//        let cancelable = board
-//            .$isGameover
-//            .sink { isGameOver in
-//                result = isGameOver
-//                callCount += 1
-//            }
-//
-//        XCTAssertFalse(result, "initially the game should be ongoing")
-//
-//        for i in 0...8 {
-//            board.squares[i].occupiedBy = .home
-//        }
-//
-//        XCTAssertEqual(
-//            board.squares.map(\.occupiedBy),
-//            [.home, .home, .home, .home, .home, .home, .home, .home, .home]
-//        )
-//
-//        XCTAssertEqual(callCount, 2)
-//
-//        XCTAssertTrue(result, "A game ends if there are no more boxes available or if one of the two players align three symbols before the other.")
-//        cancelable.cancel()
-//    }
+    func test_board_gameover() throws {
+        var result = false
+        var callCount = 0
+        let cancelable = board
+            .$isGameover
+            .sink { isGameOver in
+                result = isGameOver
+                callCount += 1
+            }
+
+        XCTAssertFalse(result, "initially the game should be ongoing")
+
+        let row: [IndexPath] = [.init(item: 0, section: 0), .init(item: 1, section: 0), .init(item: 2, section: 0) ]
+        let indexRows: [[IndexPath]] = [row, row, row]
+        for row in indexRows {
+            for indexPath in row {
+                board.occupy(at: indexPath, with: .home)
+            }
+        }
+
+        XCTAssertEqual(callCount, 2)
+
+        XCTAssertTrue(result, "A game ends if there are no more boxes available or if one of the two players align three symbols before the other.")
+        cancelable.cancel()
+    }
 //
 //    func test_board_almost_gameover() throws {
 //        var result = false
