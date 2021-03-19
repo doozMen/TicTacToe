2529acfa50a8401692a9fe883fe3736d774a6f0c
kata-me <kata@dooz.be>
2021-03-19 17:08:40 +0100

    add occupy function and make squares private

--- TicTacToe/Shared/SharedPackage/Sources/SharedPackage/Board.swift
+++ TicTacToe/Shared/SharedPackage/Sources/SharedPackage/Board.swift
@@ -1,30 +1,32 @@
 import Foundation
 import Combine
 
-final class Board {
-    let squares: [Square]
+final class Board: ObservableObject {
+    private let squares: [[Square]]
     
     @Published var isGameover: Bool
-    
-    private var squareCancelables = [AnyCancellable]()
-    
+    @Published var isStarted: Bool
+        
     init() {
-        var squares = [Square]()
-        for _ in 0...8 { squares.append(.init(.nobody)) }
-        self.squares = squares
+        self.squares = [
+            [.init(.nobody), .init(.nobody), .init(.nobody)],
+            [.init(.nobody), .init(.nobody), .init(.nobody)],
+            [.init(.nobody), .init(.nobody), .init(.nobody)],
+        ]
         self.isGameover = false
-        squares.forEach {
-            squareCancelables
-                .append(
-                    $0.$occupiedBy
-                        .sink { [weak self] _ in self?.checkGameStatus() }
-                )
-        }
+        self.isStarted = false
     }
+    
+    
+    func occupy(at indexPath: IndexPath, with player: Square.OccupiedBy) {
+        
+    }
+    
     private func checkGameStatus() {
         guard !isGameover else { return }
         
-        let unOccupied = squares.first { $0.occupiedBy == .nobody }
+        let flatSquares: [Square] = squares.flatMap { $0 }
+        let unOccupied = flatSquares.first { $0.occupiedBy != .nobody }
         
         if unOccupied == nil {
             isGameover = true
--- TicTacToe/Shared/SharedPackage/Tests/SharedPackageTests/BoardTests.swift
+++ TicTacToe/Shared/SharedPackage/Tests/SharedPackageTests/BoardTests.swift
@@ -15,64 +15,61 @@ final class BoardTests: XCTestCase {
     }
     
     func test_board_initial_nobody_on_any_square() {
-        let result = board.squares.map(\.occupiedBy)
-        XCTAssertEqual(result, Array(repeating: Square.OccupiedBy.nobody, count: 9))
+        XCTAssertFalse(board.isStarted)
     }
     
     func test_board_started() throws {
-        board.squares.first?.occupiedBy = .home
-        XCTAssertEqual(
-            board.squares.map(\.occupiedBy),
-            [.home, .nobody, .nobody, .nobody, .nobody, .nobody, .nobody, .nobody, .nobody]
-        )
+        board.occupy(at: .init(item: 0, section: 1), with: .home)
+        
+        XCTAssertTrue(board.isStarted)
     }
     
-    func test_board_gameover() throws {
-        var result = false
-        var callCount = 0
-        let cancelable = board
-            .$isGameover
-            .sink { isGameOver in
-                result = isGameOver
-                callCount += 1
-            }
-        
-        XCTAssertFalse(result, "initially the game should be ongoing")
-        
-        for i in 0...8 {
-            board.squares[i].occupiedBy = .home
-        }
-        
-        XCTAssertEqual(
-            board.squares.map(\.occupiedBy),
-            [.home, .home, .home, .home, .home, .home, .home, .home, .home]
-        )
-        
-        XCTAssertEqual(callCount, 2)
-        
-        XCTAssertTrue(result, "A game ends if there are no more boxes available or if one of the two players align three symbols before the other.")
-        cancelable.cancel()
-    }
-    
-    func test_board_almost_gameover() throws {
-        var result = false
-        let cancelable = board
-            .$isGameover
-            .sink { isGameOver in
-                result = isGameOver
-            }
-        
-        XCTAssertFalse(result, "initially the game should be ongoing")
-        
-        for i in 0...1 {
-            board.squares[i].occupiedBy = .home
-        }
-        
-        XCTAssertEqual(
-            board.squares.map(\.occupiedBy),
-            [.home, .home, .nobody, .nobody, .nobody, .nobody, .nobody, .nobody, .nobody]
-        )
-        XCTAssertFalse(result, "A game ends if there are no more boxes available or if one of the two players align three symbols before the other.")
-        cancelable.cancel()
-    }
+//    func test_board_gameover() throws {
+//        var result = false
+//        var callCount = 0
+//        let cancelable = board
+//            .$isGameover
+//            .sink { isGameOver in
+//                result = isGameOver
+//                callCount += 1
+//            }
+//        
+//        XCTAssertFalse(result, "initially the game should be ongoing")
+//        
+//        for i in 0...8 {
+//            board.squares[i].occupiedBy = .home
+//        }
+//        
+//        XCTAssertEqual(
+//            board.squares.map(\.occupiedBy),
+//            [.home, .home, .home, .home, .home, .home, .home, .home, .home]
+//        )
+//        
+//        XCTAssertEqual(callCount, 2)
+//        
+//        XCTAssertTrue(result, "A game ends if there are no more boxes available or if one of the two players align three symbols before the other.")
+//        cancelable.cancel()
+//    }
+//    
+//    func test_board_almost_gameover() throws {
+//        var result = false
+//        let cancelable = board
+//            .$isGameover
+//            .sink { isGameOver in
+//                result = isGameOver
+//            }
+//        
+//        XCTAssertFalse(result, "initially the game should be ongoing")
+//        
+//        for i in 0...1 {
+//            board.squares[i].occupiedBy = .home
+//        }
+//        
+//        XCTAssertEqual(
+//            board.squares.map(\.occupiedBy),
+//            [.home, .home, .nobody, .nobody, .nobody, .nobody, .nobody, .nobody, .nobody]
+//        )
+//        XCTAssertFalse(result, "A game ends if there are no more boxes available or if one of the two players align three symbols before the other.")
+//        cancelable.cancel()
+//    }
 }
