87b9a8a6db7039999812aa0ce78f2149a0fed795
kata-me <kata@dooz.be>
2021-03-20 16:41:17 +0100

    add failing test for accesibility lable to change when circle is selected

---
+++ TicTacToeUITests (iOS)/TicTacToeUITests__iOS_.swift
@@ -0,0 +1,38 @@
+//
+//  TicTacToeUITests__iOS_.swift
+//  TicTacToeUITests (iOS)
+//
+//  Created by Stijn Willems on 20/03/2021.
+//
+
+import XCTest
+
+class TicTacToeUITests__iOS_: XCTestCase {
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func test_tap_circles_reset() throws {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let resetButton = app.buttons["Reset"]
+        XCTAssertTrue(resetButton.waitForExistence(timeout: 2))
+        
+        let firstCircle = app.otherElements["Square-0"]
+        XCTAssertTrue(firstCircle.waitForExistence(timeout: 2))
+        firstCircle.tap()
+        XCTAssertEqual(firstCircle.label, "Square 1 - from you")
+        resetButton.tap()
+        XCTAssertEqual(firstCircle.label, "Square 1 - from nobody yet")
+    }
+
+}
