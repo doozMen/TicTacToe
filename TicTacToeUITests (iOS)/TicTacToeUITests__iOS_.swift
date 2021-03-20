ceaf86fb7510bbad04e73100dd770ab2ba10c46d
kata-me <kata@dooz.be>
2021-03-20 18:28:34 +0100

    only need one test target for UITests, loging tests are in kit

--- TicTacToeUITests (iOS)/TicTacToeUITests__iOS_.swift
+++ TicTacToeUITests (iOS)/TicTacToeUITests__iOS_.swift
@@ -37,5 +37,13 @@ class TicTacToeUITests__iOS_: XCTestCase {
         XCTAssertTrue(gameStatusTextView.waitForExistence(timeout: 2))
         XCTAssertEqual(gameStatusTextView.label, "no winner yet")
     }
-
+    
+    func testLaunchPerformance() throws {
+        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
+            // This measures how long it takes to launch your application.
+            measure(metrics: [XCTApplicationLaunchMetric()]) {
+                XCUIApplication().launch()
+            }
+        }
+    }
 }
--- TicTacToeUITests (macOS)/TicTacToeUITests__macOS_.swift
+++ TicTacToeUITests (macOS)/TicTacToeUITests__macOS_.swift
@@ -1,6 +1,6 @@
 //
-//  TicTacToeUITests__macOS_.swift
-//  TicTacToeUITests (macOS)
+//  TicTacToeUITests__iOS_.swift
+//  TicTacToeUITests (iOS)
 //
 //  Created by Stijn Willems on 20/03/2021.
 //
@@ -8,29 +8,36 @@
 import XCTest
 
 class TicTacToeUITests__macOS_: XCTestCase {
-
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
-
+    
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() throws {
+    
+    func test_tap_circles_reset() throws {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
         app.launch()
-
+        
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let resetButton = app.buttons["Reset"]
+        XCTAssertTrue(resetButton.waitForExistence(timeout: 2))
+        
+        let firstCircle = app.otherElements["Square-0"]
+        XCTAssertTrue(firstCircle.waitForExistence(timeout: 2))
+        firstCircle.tap()
+        XCTAssertEqual(firstCircle.label, "Square 1 - from you")
+        resetButton.tap()
+        XCTAssertEqual(firstCircle.label, "Square 1 - from nobody yet")
+        let gameStatusTextView = app.staticTexts["Game Status Text"]
+        XCTAssertTrue(gameStatusTextView.waitForExistence(timeout: 2))
+        XCTAssertEqual(gameStatusTextView.value as? String, "no winner yet")
     }
-
+    
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
             // This measures how long it takes to launch your application.
